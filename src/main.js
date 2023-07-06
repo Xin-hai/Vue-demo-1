58d962d9d0be3edf7ef7f23554da640e99156813
Xin-hai <aa1940358283@163.com>
2023-07-06 10:24:24 +0800

    init Vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,36 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import Vue from 'vue'
+// import App from './App.vue'
 
+// import Vue from 'vue/dist/vue.js';
+
+
+import  Demo from './demo.vue'
+
+const Vue = window.Vue
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+
+
+const vm = new Vue({
+    components: {Demo},
+    data: {
+        visible: true
+    },
+    template: `
+      <div>
+      <button @click="toggle">toggle</button>
+      <hr>
+      <Demo v-if="visible === true"/>
+      </div>
+    `,
+    methods: {
+        toggle(){
+            this.visible = !this.visible
+        }
+    }
+})
+
+vm.$mount('#xinhai')
+
+// 组件可以组合的物件
+
